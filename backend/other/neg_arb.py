2c8810cbdd9be041ff72521f0aae68d26890c1f8
jdmnk <5247767+jdmnk@users.noreply.github.com>
2025-11-02 23:40:03 +0100

    neg arbi with books (wip)

--- backend/other/neg_arb.py
+++ backend/other/neg_arb.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 from src.polymarket.poly_client import PolyClient
+from src.polymarket.poly_client_prices import PolyClientPrices
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
@@ -10,39 +11,33 @@ logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 async def main() -> None:
     poly_client = PolyClient()
+    poly_client_prices = PolyClientPrices()
     active_events = await poly_client.get_active_events(count=500)
     negrisk_events = [e for e in active_events if e.get("negRisk")]
 
     for negrisk_event in negrisk_events:
         sumBuy = 0
-        sumSell = 0
         yesTokenIds = []
-        noTokenIds = []
+        # noTokenIds = []
         # markets = [market["conditionId"] for market in negrisk_event["markets"]]
         for market in negrisk_event["markets"]:
             token_ids = json.loads(market["clobTokenIds"])
             yesTokenIds.append(token_ids[0])
-            noTokenIds.append(token_ids[1])
+            # noTokenIds.append(token_ids[1])
 
-        prices_requests = [{"token_id": token_id, "side": "BUY"} for token_id in yesTokenIds]
-        yes_prices = await poly_client.get_market_prices_by_request(prices_requests)
-        no_prices_requests = [{"token_id": token_id, "side": "SELL"} for token_id in noTokenIds]
-        no_prices = await poly_client.get_market_prices_by_request(no_prices_requests)
+        books_requests = [{"token_id": token_id} for token_id in yesTokenIds]
+        yes_books = await poly_client_prices.get_order_books_by_request(books_requests)
 
         print("Event: " + negrisk_event["title"])
         print("Number of outcomes: " + str(len(negrisk_event["markets"])))
 
         # iterate prices dict and sum up the prices for each side
-        for _token_id, _prices in yes_prices.items():
-            # print("YES: ", _prices)
-            sumBuy += float(_prices.get("BUY", 0))
-
-        for _token_id, _prices in no_prices.items():
-            # print("NO: ", _prices)
-            sumSell += float(_prices.get("SELL", 0))
+        for _book in yes_books:
+            bids = _book.get("bids")
+            if bids:
+                sumBuy += float(bids[-1].get("price", 0))
 
         print("Yes sum: " + str(sumBuy))
-        print("No sum: " + str(len(negrisk_event["markets"]) - sumSell))
         print("--------------------------------")
 
 
