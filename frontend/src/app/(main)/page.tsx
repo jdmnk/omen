b318257e9be38e078a9c3f12c6578b467c901479
jdmnk <5247767+jdmnk@users.noreply.github.com>
2026-01-01 23:44:28 +0100

    new root page

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,15 +1,5 @@
-import { UserSearchBar } from "@/modules/user/components/UserSearchBar";
-import { UserWatchlist } from "@/modules/user/components/UserWatchlist";
+import { MarketLayout } from "@/modules/market_new/components/MarketLayout";
 
 export default function Home() {
-  return (
-    <div className="bg-background">
-      <main className="w-full">
-        <div className="container mx-auto max-w-7xl p-6 space-y-6">
-          <UserSearchBar />
-          <UserWatchlist />
-        </div>
-      </main>
-    </div>
-  );
+  return <MarketLayout />;
 }
